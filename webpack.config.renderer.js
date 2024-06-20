7ea0cdbfea61a54702e85a9cbeca94c6e3c322f4
lekkala.sai <lekkala.sai@nxtgen.com>
2024-06-20 11:56:30 +0530

    changed mattermost desktop app name

--- webpack.config.renderer.js
+++ webpack.config.renderer.js
@@ -39,7 +39,7 @@ module.exports = merge(base, {
     },
     plugins: [
         new HtmlWebpackPlugin({
-            title: 'Mattermost Desktop App',
+            title: 'ECI',
             template: 'src/renderer/index.html',
             chunks: ['index'],
             filename: 'index.html',
